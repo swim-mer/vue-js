ab7d4ccd943258d05ed9ca762d47c7b6c9fd5666
Kelly Hodis <kelly.hodis@processmaker.com>
2020-04-15 14:19:14 -0700

    Separate HTML and JS

---
+++ apps/apps.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#vue-app',
+  data: {
+    message: 'displayed text'
+  }
+})
+var hover = new Vue({
+  el: '#hover-app',
+  data: {
+    message: 'hovering text'
+  }
+})
+
