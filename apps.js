0a604f817cbebe115b71762cff77b49c726beb8c
Kelly Hodis <kelly.hodis@processmaker.com>
2020-04-15 14:25:12 -0700

    Add comments

--- apps.js
+++ apps.js
@@ -1,6 +1,9 @@
+// Create new app
 var app = new Vue({
+  // connects to div id 'vue-app'
   el: '#vue-app',
   data: {
+    // given to 'message' variable in HTML
     message: 'displayed text'
   }
 })
