5cd4ac68433055dc7c7369c61f4bff6b08079f89
Kelly Hodis <kelly.hodis@processmaker.com>
2020-04-16 11:10:36 -0700

    Add first Vue project

---
+++ HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, World!'
+    }
+  }
+}
+</script>
+<!-- Helloworld project from https://www.youtube.com/watch?v=a3TISMIWzP8 -->
