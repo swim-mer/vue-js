36ebd759fe57e1dbca803b759f92ba22be4216de
Kelly Hodis <kelly.talk.api@gmail.com>
2021-05-11 17:14:06 -0400

    Update App component

--- basics/new-app/src/App.vue
+++ basics/new-app/src/App.vue
@@ -1,26 +1,49 @@
+<!-- COMPONENT -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ msg }}
+    <Stars v-bind:stars="stars" />
+    <Planets v-bind:planets="planets" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Stars from './components/Stars';
+import Planets from './components/Planets';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Stars,
+    Planets
+  },
+  data() {
+    return {
+      stars: [
+        {
+          id: 1,
+          title: "Alpha Centauri",
+          alive: true,
+          planets: true
+        },
+        {
+          id: 2,
+          title: "WD 1856",
+          alive: false,
+          planets: true
+        },
+        {
+          id: 3,
+          title: "Proxima Centauri",
+          alive: true,
+          planets: true
+        }
+      ],
+      msg: 'Basically, hey world, sup'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
