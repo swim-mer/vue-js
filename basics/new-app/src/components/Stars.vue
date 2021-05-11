dbdefda8372cdd745b450a0c179c725a5303b228
Kelly Hodis <kelly.talk.api@gmail.com>
2021-05-11 17:14:28 -0400

    Add Stars component

---
+++ basics/new-app/src/components/Stars.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Stars</h1>
+    <div v-bind:key="star.id" v-for="star in stars">
+      <h3>{{ star.title }}</h3>
+      <Planets v-bind:star="star" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Planets from './Planets.vue';
+
+export default {
+  name: "Stars",
+  components: {
+    Planets
+  },
+  props: ["stars"]
+}
+</script>
+
+<style scoped>
+
+</style>
