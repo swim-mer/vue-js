ead0f9e4d80cd122eccaefefd1301e609d310407
Kelly Hodis <khodis@geisel-software.com>
2021-05-25 00:00:49 -0400

    Add Nuxt project

---
+++ basics/component-interactions/component-interaction/components/Cat.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <Pet animal_type="cat" msg="Annie"></Pet>
+    </div>
+</template>
+
+<script>
+import Pet from '~/components/Pet'
+export default {
+  components: { Pet }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ basics/component-interactions/component-interaction/components/Pet.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h1>My pet is a {{ animal_type }} named {{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        msg: {
+            type: String,
+            default: ''
+        },
+        animal_type: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
