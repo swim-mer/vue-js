a2e1b979ca9b9d46a3581fc133f1f04fab329b15
Kelly Hodis <khodis@geisel-software.com>
2021-06-14 21:14:42 -0400

    Add exercise

---
+++ basics/events/fun-with-events/src/components/Child.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <img v-if="showImage"
+             alt="Avocado seedling"
+             src="../assets/istvan-hernek-a1B4e-gGPS8-unsplash.jpg"
+        >
+        <p>Click this button:
+            <button @click="$emit('status', true)">
+                Click Me To See Child Component
+            </button>
+            <button @click="$emit('status', false)">
+                Reset
+            </button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  props: {
+      showImage: {
+          type: Boolean,
+          default: false
+      }
+  }
+}
+</script>
+
+<style scoped>
+img {
+    height: 400px;
+}
+</style>
---
+++ basics/events/fun-with-events/src/components/Parent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <img alt="Mountainous forests of Adjara, Georgia" 
+             src="../assets/marita-kavelashvili-ugnrXk1129g-unsplash.jpg"
+        >
+        <Child @status="showImage=$event" :showImage="showImage">
+        </Child>
+    </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+  name: 'Parent',
+  components: { Child },
+  data: () => ({
+    showImage: false
+  }),
+  props: {
+    msg: {
+        type: String,
+        default: ''
+    }
+  },
+  methods: {
+      display(event) {
+          console.log(event)
+          this.displayImage = event
+      }
+  }
+}
+</script>
+
+<style scoped>
+img {
+    height: 400px;
+}
+</style>
